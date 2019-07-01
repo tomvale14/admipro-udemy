c0b2a7d1829f74eb6b8270da0ba2f2f2adc12231
tomvale14 <minsanto@gmail.com>
2019-07-01 22:05:40 +0200

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,39 +4,26 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
+// Módulos
+import { PagesModule } from './pages/pages.modules';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgessComponent } from './pages/progess/progess.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgessComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent
+    RegisterComponent
   ],
 
   // Nota: los Módulos van siempre en la parte de los imports
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgessComponent } from './pages/progess/progess.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 // ==============================================================
@@ -12,20 +8,21 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
 // ==============================================================
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,                    // Página Principal
-        children: [                                   // router-outlet de las páginas hijas de pages.component
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgessComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
+    // {
+    //     path: '',
+    //     component: PagesComponent,                    // Página Principal
+    //     children: [                                   // router-outlet de las páginas hijas de pages.component
+    //         { path: 'dashboard', component: DashboardComponent },
+    //         { path: 'progress', component: ProgessComponent },
+    //         { path: 'graficas1', component: Graficas1Component },
+    //         { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    //     ]
+    // },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
+// forRoot son router-outlet principales
 // Posteriormente se importa en app.module.ts
-export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true });
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
